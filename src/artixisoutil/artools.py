4cf125c3b95a6edcbdd1d83d24301e68ea1d44b0
LowByteNeko <lowbyteneko@gmail.com>
2023-07-18 23:06:09 +0200

    project initialization done

--- src/artixisoutil/artools.py
+++ src/artixisoutil/artools.py
@@ -1,7 +1,6 @@
 from .pacman import check_pkgs
 from .logger import logError, logSuccess
-from .configs import baseGen, isoGen
-from os import mkdir, getenv, path, getcwd, chdir
+from os import mkdir, getenv, path, chdir
 from shutil import copy
 
 
@@ -14,9 +13,9 @@ def getMangedInput(prompt, default, check, failMsg):
     val = ""
     while not check(val):
         try:
-            val = input(prompt)
+            val = input(prompt).strip()
             if not check(val):
-                print(failMsg)
+                failMsg(val)
         except:
             val = default
     return val
@@ -43,20 +42,64 @@ def prepare():
     logSuccess("Basic artools config initialized")
 
 
+def checkStrength(prompt):
+    try:
+        val = int(prompt)
+        return not (val > 22 or val < 1)
+    except ValueError:
+        return False
+
+
 def init_proj():
     name = getMangedInput("Type name of your project: ", "",
-                          lambda p: len(p.strip()) > 0, "Enter project name")
-    name = name.strip()
+                          lambda p: len(p.strip()) > 0,
+                          lambda _: print("Enter valid project name!"))
 
     if path.exists(name):
         logError(f"Directory \"{name}\" already exists!")
 
     mkdir(name)
     chdir(name)
-    cwd = getcwd()
-    home = getenv("HOME")
+    version = getMangedInput("Type version of your project: ",
+                             "unknown",
+                             lambda p: len(p.strip()) > 0,
+                             lambda _: print("Enter valid project version!"))
 
-    with open(f"{home}/.config/artools/artools-base.conf", "w") as f:
-        f.write(baseGen(cwd))
+    inits = ["openrc", "runit", "s6", "suite66", "dinit"]
+    msg = "Select your init system:\n" + "\n".join(inits) + ": "
+
+    init = getMangedInput(msg, "openrc", lambda p: p in inits,
+                          lambda p:
+                          print(f"Init system \"{p}\" is invalid option"))
+
+    compr = getMangedInput("Type desired compression [zstd/xz]: ",
+                           "zstd",
+                           lambda p: p in ["zstd", "xz"],
+                           lambda p: print(f"Unsupported compression \"{p}\""))
+
+    strength = "1"
+
+    if compr == "zstd":
+        strength =\
+                int(getMangedInput("Type zstd compressions strength 1..22: ",
+                                   "1",
+                                   lambda p: checkStrength(p),
+                                   lambda _:
+                                   print("Invalid compression strength!")))
+
+    with open("./workspace.toml", "w") as f:
+        f.write(f"""[iso]
+version = "{version}"
+
+[iso.compression]
+type = "{compr}"
+level = {strength}
+
+[iso.init]
+type = "{init}"
+services = ["acpid", "cronie", "metalog"]
+""")
 
     chdir("..")
+
+    logSuccess("Project initialized!")
