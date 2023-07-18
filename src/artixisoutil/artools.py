c36163a0eecb6b88005387503a603884c5722aab
LowByteNeko <lowbyteneko@gmail.com>
2023-07-18 22:10:41 +0200

    artools now can be configured

--- src/artixisoutil/artools.py
+++ src/artixisoutil/artools.py
@@ -1,6 +1,12 @@
 from .pacman import check_pkgs
-from .logger import logError
+from .logger import logError, logSuccess
 from os import mkdir, getenv, path
+from shutil import copy
+
+
+def copyIfNot(src, dest):
+    if not path.exists(dest):
+        copy(src, dest)
 
 
 def prepare():
@@ -9,5 +15,16 @@ def prepare():
 
     home = getenv("HOME")
 
-    if not path.exists(f"{home}/artools-workspace"):
-        mkdir(f"{home}/artools-workspace")
+    if not path.exists(f"{home}/.config/artools/"):
+        mkdir(f"{home}/.config/artools/")
+
+    copyIfNot("/etc/artools/artools-base.conf",
+              f"{home}/.config/artools/artools-base.conf")
+
+    copyIfNot("/etc/artools/artools-iso.conf",
+              f"{home}/.config/artools/artools-iso.conf")
+
+    copyIfNot("/etc/artools/artools-pkg.conf",
+              f"{home}/.config/artools/artools-pkg.conf")
+
+    logSuccess("Basic artools config initialized")
--- src/artixisoutil/logger.py
+++ src/artixisoutil/logger.py
@@ -2,6 +2,7 @@ import sys
 
 COLOR_RED = "\x1b[31m"
 COLOR_RESET = "\x1b[0m"
+COLOR_GREEN = "\x1b[32m"
 
 
 def eprint(*args, **kwargs):
@@ -11,3 +12,7 @@ def eprint(*args, **kwargs):
 def logError(msg: str):
     eprint(f"[{COLOR_RED}FAIL{COLOR_RESET}]: {msg}")
     exit(1)
+
+
+def logSuccess(msg: str):
+    print(f"[{COLOR_GREEN}DONE{COLOR_RESET}]: {msg}")
